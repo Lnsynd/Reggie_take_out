8de1092339ecfeda3d030c1a1c6ec9c28da22741
lqs <2034215871@qq.com>
2022-11-08 21:01:31 +0800

    +02_启用、禁用员工账号，编辑员工信息

--- src/main/java/com/lqs/reggie_take_out/config/WebMvcConfig.java
+++ src/main/java/com/lqs/reggie_take_out/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.lqs.reggie_take_out.config;
 
+import com.lqs.reggie_take_out.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 
 @Slf4j
@@ -19,5 +24,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+
+    /**
+     * 扩展Mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 1、创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        // 2、设置对象转换器，将Java对象转换为JSON
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        converters.add(0,messageConverter);
     }
 }
--- src/main/java/com/lqs/reggie_take_out/controller/EmployeeController.java
+++ src/main/java/com/lqs/reggie_take_out/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.lqs.reggie_take_out.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lqs.reggie_take_out.common.R;
 import com.lqs.reggie_take_out.entity.Employee;
 import com.lqs.reggie_take_out.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,12 +25,13 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest request,  @RequestBody Employee employee) {
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
 
         // 1、将页面提交的密码password进行md5加密
         String password = employee.getPassword();
@@ -39,48 +39,50 @@ public class EmployeeController {
 
         // 2、根据页面提交的用户名查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
         // 3、如果查询不到返回登录失败
-        if(emp ==null){
+        if (emp == null) {
             return R.error("登录失败!");
         }
 
         // 4、密码比对，不一致则返回登陆失败结果
-        if(!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("登录失败!");
         }
 
         // 5、状态比对，如果已经禁用返回失败结果
-        if(emp.getStatus()==(0)){
+        if (emp.getStatus() == (0)) {
             return R.error("账号已禁用!");
         }
 
         // 6、登陆成功,将员工id存入session，并返回登录成功
-        request.getSession().setAttribute("employee",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
 
     }
 
     /**
      * 员工退出
+     *
      * @return
      */
     @PostMapping("/logout")
-    public R<String> logout(HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功!");
     }
 
     /**
      * 新增员工
+     *
      * @param employee
      * @return
      */
     @PostMapping
-    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
-        log.info("新增员工信息:{}",employee.toString());
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工信息:{}", employee.toString());
 
         // 设置初始密码，需要进行加密
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
@@ -96,5 +98,61 @@ public class EmployeeController {
         employeeService.save(employee);
 
         return R.success("新建员工成功!");
+    }
+
+    /**
+     * 员工信息分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page:{},pageSize:{},name:{}", page, pageSize, name);
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        // 条件构造器
+        LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.like(StringUtils.isNotBlank(name), Employee::getName, name);
+        lambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行查询
+        employeeService.page(pageInfo, lambdaQueryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id修改员工信息
+     *
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功!");
+    }
+
+    /**
+     * 根据Id查询用户信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        if(employee!=null){
+            return R.success(employee);
+        }else{
+            return R.error("没有该用户信息");
+        }
+
     }
 }
